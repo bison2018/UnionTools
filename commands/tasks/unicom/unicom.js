355ba55f4b2ea1aff7132a69bd7bd05f7dc039ba
simo8102 <simo8102@protonmail.com>
2021-04-16 04:26:38 +0800

    添加自动兑换1G流量

--- commands/tasks/unicom/unicom.js
+++ commands/tasks/unicom/unicom.js
@@ -89,14 +89,6 @@ var start = async (params) => {
     await require('./dailyBookLuckdraw').doTask(request, options)
   }, taskOption)
 
-  // 首页-签到有礼-免费领-1G流量日包
-  await scheduler.regTask('daily1GFlowTask', async (request) => {
-    await require('./daily1GFlowTask').doTask(request, options)
-  }, {
-    ...taskOption,
-    startTime: 20 * 3600,
-    ignoreRelay: true
-  })
 
   // 首页-签到有礼-免费领-浏览领积分
   await scheduler.regTask('dailyLiuLan', async (request) => {
